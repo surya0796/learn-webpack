95dfdfd321983402b2a5cf717ee4c4ee9bc96a22
Surya <ssaini@demandbase.com>
2024-03-24 01:36:16 +0530

    added webpack dev server, removed unecessary packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
+const webpack = require("webpack");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = ({ mode }) => {
   return {
     mode,
     output: {
       filename: "bundle.js",
     },
+    plugins: [new htmlWebpackPlugin(), new webpack.ProgressPlugin()],
   };
 };
