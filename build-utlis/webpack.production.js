f1af7adbffac1a70aef3f242da2ac8c1959a8f09
Surya <ssaini@demandbase.com>
2024-03-24 14:19:37 +0530

    splitted config into to separate env files

---
+++ build-utlis/webpack.production.js
@@ -0,0 +1,4 @@
+module.exports = (mode) => {
+  console.log(mode);
+  return {};
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 const webpack = require("webpack");
 const htmlWebpackPlugin = require("html-webpack-plugin");
+const webpackMerge = require("webpack-merge");
+const modeConfig = (mode) => require(`./build-utlis/webpack.${mode}`)(mode);
 
 module.exports = ({ mode }) => {
-  return {
-    mode,
-    output: {
-      filename: "bundle.js",
+  return webpackMerge.merge(
+    {
+      mode,
+      output: {
+        filename: "bundle.js",
+      },
+      plugins: [new htmlWebpackPlugin(), new webpack.ProgressPlugin()],
     },
-    plugins: [new htmlWebpackPlugin(), new webpack.ProgressPlugin()],
-  };
+    modeConfig(mode)
+  );
 };
